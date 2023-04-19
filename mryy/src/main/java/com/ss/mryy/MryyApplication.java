ac25323f918549f10af61b159b23f2efe5185f06
520ljs <2798377812@qq.com>
2023-04-19 21:05:48 +0800

    项目骨架创建完成

---
+++ mryy/src/main/java/com/ss/mryy/MryyApplication.java
@@ -0,0 +1,13 @@
+package com.ss.mryy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MryyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MryyApplication.class, args);
+    }
+
+}
---
+++ mryy/src/test/java/com/ss/mryy/MryyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.mryy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MryyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
