3ffa746451d9f3ad85084fc9e830b3a787f9c13c
520ljs <2798377812@qq.com>
2023-04-20 14:58:09 +0800

    swagger2 创建API应用

---
+++ mryy/src/main/java/com/ss/mryy/config/Swagger2Config.java
@@ -0,0 +1,54 @@
+package com.ss.mryy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @Author:ljy.s
+ * @Date:2023/4/20 - 04 - 20 - 11:44
+ */
+@Configuration// 让Spring来加载该配置类
+@EnableSwagger2// 启用Swagger2
+public class Swagger2Config {
+
+    /**
+     * 创建API应用
+     * apiInfo() 增加API相关信息
+     * 通过select()函数返回一个ApiSelectorBuilder实例,用来控制哪些接口暴露给Swagger来展现，
+     * 本例采用指定扫描的包路径来定义指定要建立API的目录。
+     *
+     * @return
+     */
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.ss.mryy.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * 创建该API的基本信息（这些基本信息会展现在文档页面中）
+     * 访问地址：http://项目实际地址/swagger-ui.html
+     *
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("520ljs-API接口文档")
+                .description("520ljs-API接口文档")
+                .contact(new Contact("ljs", "https://www.baidu.com", "9876123456@qq.com"))
+                .version("1.0")
+                .build();
+    }
+}
--- mryy/src/main/java/com/ss/mryy/controller/BusinessController.java
+++ mryy/src/main/java/com/ss/mryy/controller/BusinessController.java
@@ -2,6 +2,8 @@ package com.ss.mryy.controller;
 
 import com.ss.mryy.entity.Business;
 import com.ss.mryy.service.BusinessService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import javax.annotation.Resource;
  * @author makejava
  * @since 2023-04-20 01:52:40
  */
+@Api(tags = "商家接口") // 类上 指定 中文名
 @RestController
 @RequestMapping("business")
 public class BusinessController {
@@ -42,6 +45,7 @@ public class BusinessController {
      * @param id 主键
      * @return 单条数据
      */
+    @ApiOperation(value = "查询单个商家信息", notes = "通过主键查询单条数据")// 方法上 指定 中文名
     @GetMapping("{id}")
     public ResponseEntity<Business> queryById(@PathVariable("id") Long id) {
         return ResponseEntity.ok(this.businessService.queryById(id));
